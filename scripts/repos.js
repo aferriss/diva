7f778d91c814ffc5a8e0fb33f1af94cc90152da9
aferriss <aferriss@gmail.com>
2015-08-13 00:47:23 -0700

    adding repos working

--- scripts/repos.js
+++ scripts/repos.js
@@ -7,7 +7,13 @@ video.width = w;
 video.height = h;
 var time = 0;
 
-var scene, camScene, diffScene, fbScene, renderer, camera, orthoCamera, plane, skyBox, shader, diffShader, fbShader, blurShader, shader2, tex, fbTex, sceneTex, prevFrame, videoTexture, videoImage, videoImageContext;
+var scene, camScene, diffScene, fbScene;
+var renderer;
+var camera, orthoCamera;
+var plane, skyBox;
+var shader, diffShader, fbShader, blurShader, shader2;
+var tex, fbTex, sceneTex, prevFrame; 
+var videoTexture, videoImage, videoImageContext;
 var sheets = [];
 var mouseX, mouseY;
 var box, quad;
@@ -27,6 +33,14 @@ var combineShader, fs2;
 var sepTex;
 var inc = 0;
 var pastFrame;
+var reposShader;
+var diffShader;
+var colorShader;
+
+
+var scene1, scene2, scene3, scene4, scene5;
+
+var rtt, tex1, tex2, tex3, diffTex;
 
 
 function init(){
@@ -34,6 +48,7 @@ function init(){
   video.src = "images/allOne.mp4";
   video.load();
   video.play();
+  video.volume = 0.0;
 
   videoImage = document.createElement('canvas');
   videoImage.width = 1280;
@@ -41,9 +56,13 @@ function init(){
 
   videoImageContext = videoImage.getContext('2d');
   
-  //initWebcam();
   camScene = new THREE.Scene();
   sepScene = new THREE.Scene();
+  scene1 = new THREE.Scene();
+  scene2 = new THREE.Scene();
+  scene3 = new THREE.Scene();
+  scene4 = new THREE.Scene();
+  scene5 = new THREE.Scene();
 
   orthoCamera = new THREE.OrthographicCamera( w/-2, w/2, h/2, h/-2, -10000, 10000);
   camera = new THREE.PerspectiveCamera(45, w/h, 0.1,40000);
@@ -51,72 +70,93 @@ function init(){
   camScene.add(orthoCamera);
   sepScene.add(camera);
 
+  scene1.add(orthoCamera);
+  scene2.add(orthoCamera);
+  scene3.add(orthoCamera);
+  scene4.add(orthoCamera);
+  scene5.add(orthoCamera);
 
   videoTexture = new THREE.Texture( videoImage );
   videoTexture.minFilter = THREE.LinearFilter;
   videoTexture.magFilter = THREE.LinearFilter;
 
-  console.log(videoTexture);
-
   sepTex = new THREE.WebGLRenderTarget(w, h, {minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat });
   pastFrame = new THREE.WebGLRenderTarget(w, h, {minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat });
+  tex1 = new THREE.WebGLRenderTarget( w,h, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat });
+  tex2 = new THREE.WebGLRenderTarget( w,h, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat });
+  tex3 = new THREE.WebGLRenderTarget( w,h, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat });
 
-  var screenGeometry = new THREE.PlaneGeometry(w, h);
-  planeMaterial = new THREE.MeshBasicMaterial({map:videoTexture});
-  var basicMat = new THREE.MeshPhongMaterial({color:0xffff00});
-
-  var movieMaterial = new THREE.MeshBasicMaterial({map: videoTexture, overdraw: true, side:THREE.DoubleSide})
+  rtt = new THREE.WebGLRenderTarget( w,h, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat });
+  diffTex = new THREE.WebGLRenderTarget( w,h, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat });
   
-  shader = new THREE.ShaderMaterial({
+
+  
+  diffShader = new THREE.ShaderMaterial({
     uniforms:{
       u_image: {type: 't', value: videoTexture},
-      pastFrame: {type: 't', value: pastFrame }
+      pastFrame: {type: 't', value: pastFrame },
+      fbTex: {type: 't', value: tex1 }
     },
-    vertexShader: document.getElementById('vertexShader').textContent,
-    fragmentShader: document.getElementById('passThrough').textContent
+    vertexShader: document.getElementById('vertZoom').textContent,
+    fragmentShader: document.getElementById('diff').textContent
   });
 
-  shader.side = THREE.DoubleSide;
-  shader.overdraw = true;
+  reposShader = new THREE.ShaderMaterial({
+    uniforms:{
+      tex: {type: 't', value: tex1},
+      tex2: {type: 't', value: tex3 },
+      amt: {type: 'v2', value: new THREE.Vector2( 3.8/w, 3.8/h) },
+        time: {type: 'f', value: inc}
+      },
+      vertexShader: document.getElementById('vertexShader').textContent,
+      fragmentShader:document.getElementById('repos').textContent
+  });
 
+  colorShader = new THREE.ShaderMaterial({
+    uniforms:{
+      u_image: { type: 't', value: rtt}
+    },
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader:document.getElementById('colorMap').textContent
+  });
+
+
+
+  diffShader.side = THREE.DoubleSide;
+  diffShader.overdraw = true;
+
+
+  var screenGeometry = new THREE.PlaneGeometry(w, h);
+
+
+
+  quad = new THREE.Mesh(screenGeometry, diffShader);
+  sepScene.add(quad);
+  
   planeMaterial = new THREE.MeshBasicMaterial({map:videoTexture});
-
-  var light = new THREE.PointLight({color: 0xffffff});
-  //camScene.add(light);
-  light.position.set(0,10,15);
-
-
-  
-  
-  /*
-  quad.position.set(0,0,-1000);
-  sepScene.add(quad);
-  quad.position.set(0,0,-1050);
-  sepScene.add(quad);
-  quad.position.set(0,0,-1100);
-  sepScene.add(quad);
-  quad.position.set(0,0,-1150);
-  sepScene.add(quad);
-  quad.position.set(0,0,-1200);
-  sepScene.add(quad);
-  */
-
-  
-  quad = new THREE.Mesh(screenGeometry, shader);
-  quad.position.z -= 2000;
-  
-  sepScene.add(quad);
-  
-
-  //console.log(sheets);
   quad = new THREE.Mesh(screenGeometry, planeMaterial);
   camScene.add(quad);
+
+  quad = new THREE.Mesh(screenGeometry, reposShader);
+  scene1.add(quad);
+
+  planeMaterial = new THREE.MeshBasicMaterial( {map: rtt});
+  quad = new THREE.Mesh(screenGeometry, planeMaterial);
+  scene2.add(quad);
+
+  var pplaneMaterial = new THREE.MeshBasicMaterial({map: diffTex});
+  var pquad = new THREE.Mesh(screenGeometry, pplaneMaterial);
+  scene4.add(pquad);
+
+  var cQuad = new THREE.Mesh(screenGeometry, colorShader);
+  scene5.add(cQuad);
+
+
 
   renderer = new THREE.WebGLRenderer({ preserveDrawingBuffer:false, alpha: true, antialias:false, precision: "highp"});
   renderer.setSize(window.innerWidth, window.innerHeight);
   //renderer.autoClear = false;
 
-  
 
   container.appendChild(renderer.domElement);
 
@@ -128,19 +168,14 @@ function init(){
   render();
 }
 
-var indexOfTextureToPutDataInto = 0;
 
 
 
 function render(){
   time += 0.01;
+  inc ++;
 
-  //sheets[0].rotation.y = time;
-  
-  //for(var i = 1; i<sheets.length; i++){
-  //  sheets[i].rotation.y = time*i;
-  //  sheets[i].position.z *= -10;
-  //}
+  reposShader.uniforms.time.value = inc;
 
 
   if(video.readyState === video.HAVE_ENOUGH_DATA){
@@ -154,46 +189,23 @@ function render(){
   
   controls.update();
 
+  if(inc > 100){
+  //  reposShader.uniforms.tex2.value = tex1;
+  }
   
-  
-  renderer.render(sepScene, orthoCamera);
-  renderer.render(camScene, orthoCamera, pastFrame);
+  renderer.render(sepScene, orthoCamera, diffTex, true);
+  renderer.render(camScene, orthoCamera, pastFrame, true);
+  renderer.render(scene4, orthoCamera, tex3, true);
+
+  //renderer.render(scene4, orthoCamera);
+
+  renderer.render(scene1, orthoCamera, rtt, true);
+  renderer.render(scene2, orthoCamera, tex1, true);
+  //renderer.render(scene2, orthoCamera);
+
+  renderer.render(scene5, orthoCamera);
   
   window.requestAnimationFrame(render);
-}
-
-
-
-
-function assignUVs(geometry) {
-
-
-    geometry.computeBoundingBox();
-
-    var max     = geometry.boundingBox.max;
-    var min     = geometry.boundingBox.min;
-
-    var offset  = new THREE.Vector2(0 - min.x, 0 - min.y);
-    var range   = new THREE.Vector2(max.x - min.x, max.y - min.y);
-
-    geometry.faceVertexUvs[0] = [];
-    var faces = geometry.faces;
-
-    for (i = 0; i < geometry.faces.length ; i++) {
-
-      var v1 = geometry.vertices[faces[i].a];
-      var v2 = geometry.vertices[faces[i].b];
-      var v3 = geometry.vertices[faces[i].c];
-
-      geometry.faceVertexUvs[0].push([
-        new THREE.Vector2( ( v1.x + offset.x ) / range.x , ( v1.y + offset.y ) / range.y ),
-        new THREE.Vector2( ( v2.x + offset.x ) / range.x , ( v2.y + offset.y ) / range.y ),
-        new THREE.Vector2( ( v3.x + offset.x ) / range.x , ( v3.y + offset.y ) / range.y )
-      ]);
-
-    }
-
-    geometry.uvsNeedUpdate = true;
 }
 
 
@@ -219,27 +231,3 @@ function onWindowResize() {
 
 init();
 
-
-function initWebcam(){
-  window.addEventListener('DOMContentLoaded', function(){
-        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
- 
-    if (navigator.getUserMedia) {       
-        navigator.getUserMedia({video: true, audio: false}, handleVideo, videoError);
-    }
- 
-    function handleVideo(stream) {
-      var url = window.URL || window.webkitURL;
-       video.src = url ? url.createObjectURL(stream) : stream;
-        video.play();
-        videoLoaded = true;
-        
-    }
- 
-    function videoError(e) {
-      alert('Error' + error.code);
-    }
-  });
-}
-
-//container.appendChild(video);
